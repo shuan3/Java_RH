fcb3011fcb8f1ec6cc1e2efe574106d35f72e086
Raymond Huang <shanhua18@gmail.com>
2024-08-26 22:52:30 -0400

    updated

--- D2_ObjectLiterals/promise_async_await/app.js
+++ D2_ObjectLiterals/promise_async_await/app.js
@@ -28,7 +28,7 @@ function test(){
 test()
 
 const PENDING=0;
-const FULLFILLED=1;
+const FULFILLED=1;
 const REJECTED=2;
 function CustomPromise(executor){
     let state=PENDING;
@@ -47,15 +47,41 @@ function CustomPromise(executor){
         value=err;
         catches.forEach(
         (c)=>c(err)
-        ) ;
-        this.then=function(callback){
-            if (state!==FULLFILLED) {
+        ) ;      
+        }   
+    this.then=function(callback){
+            if (state===FULFILLED) {
                 callback(value)
             }else{
                 handlers.push(callback);
             }
         
-        };          
-        }   
+        };    
         executor(resolve,reject);
     }
+
+
+const doWork=(res,rej)=>{
+    setTimeout(
+        ()=>{res("hello world")},2
+    )
+
+}
+
+let someText=new CustomPromise(doWork);
+
+someText.then((val)=>{
+    console.log("1st log: "+ val)
+})
+
+someText.then((val)=>{
+    console.log("2nd log: "+ val)
+})
+
+setTimeout(
+    ()=>{
+        someText.then((val)=>{
+            console.log("3rd log: "+ val)
+        })
+    },3
+)
